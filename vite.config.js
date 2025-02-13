e25f85998c27d45afc5a9f3f34c017f5705770b5
minced fish <39563984-muntii@users.noreply.replit.com>
2025-02-13 21:44:24 +0000

    set up for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/heysteve/', // Replace 'heysteve' with your repository name if it’s different
   server: {
     host: '0.0.0.0',
-    port: 3000 // You can change this if needed
+    port: 3000
   }
 });
